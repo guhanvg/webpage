78480e5a1d1a02662a4e239a622e4fb5ef984694
VIGNESHGUHAN E <109470075+guhanvg@users.noreply.github.com>
2022-10-17 11:10:20 +0530

    Script updated

--- script.js
+++ script.js
@@ -4,38 +4,49 @@ function validate(){
     var phone = document.getElementById("phone").value;
     var email = document.getElementById("email").value;
     var message = document.getElementById("message").value;
-    var error_message = document.getElementById("error_message");
-    
-    error_message.style.padding = "10px";
-    
+   
+       
     var text;
     var regEx=/^[A-Za-z]+$/;
     if(!name.match(regEx)){
       text = "Please Enter valid Name";
-      error_message.innerHTML = text;
+      document.querySelector('.name-error').style.display="block";
       return false;
+    }else{
+      document.querySelector('.name-error').style.display="none";
     }
     if(subject.length < 10){
       text = "Please Enter Correct Subject";
-      error_message.innerHTML = text;
+      document.querySelector('.subject-error').style.display="block";
       return false;
+    }else{
+      document.querySelector('.subject-error').style.display="none";
     }
     if(isNaN(phone) || phone.length != 10){
       text = "Please Enter valid Phone Number";
-      error_message.innerHTML = text;
+      document.querySelector('.number-error').style.display="block";
       return false;
+    }else{
+      document.querySelector('.number-error').style.display="none";
     }
+    
     var regEx=/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     if(!email.match(regEx)){
       text = "Please Enter valid Email";
-      error_message.innerHTML = text;
+      document.querySelector('.mail-error').style.display="block";
       return false;
+    }else{
+      document.querySelector('.mail-error').style.display="none";
     }
+
     if(message.length <= ""){
       text = "This page cannot be left blank!";
-      error_message.innerHTML = text;
+      document.querySelector('.message-error').style.display="block";
       return false;
+      }else{
+        document.querySelector('.message-error').style.display="none";
       }
+
     alert("Form Submitted Successfully!");
     return true;
   }
